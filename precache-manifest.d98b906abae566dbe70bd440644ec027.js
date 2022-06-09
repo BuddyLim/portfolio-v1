444cde3c8f5088d365894f876307a39ab1794114
Buddy <buddy.tlimk@gmail.com>
2022-06-09 19:36:22 +0800

    Updates

---
+++ precache-manifest.d98b906abae566dbe70bd440644ec027.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e18a87ee1fe1c114667c",
+    "url": "/portfolio/static/css/main.1e201d13.chunk.css"
+  },
+  {
+    "revision": "e18a87ee1fe1c114667c",
+    "url": "/portfolio/static/js/main.e18a87ee.chunk.js"
+  },
+  {
+    "revision": "91d1d4c5a6814aad6413",
+    "url": "/portfolio/static/js/1.91d1d4c5.chunk.js"
+  },
+  {
+    "revision": "9dccad5ad4d88ec3038b",
+    "url": "/portfolio/static/js/2.9dccad5a.chunk.js"
+  },
+  {
+    "revision": "4b1abba37b33640250dc",
+    "url": "/portfolio/static/js/runtime~main.4b1abba3.js"
+  },
+  {
+    "revision": "c20a95a6aa801cd89aa8576c1fa1db30",
+    "url": "/portfolio/static/media/headerIcon.c20a95a6.svg"
+  },
+  {
+    "revision": "94208864d506f0dc4e6917ec3187e16b",
+    "url": "/portfolio/static/media/hamburgerIcon.94208864.svg"
+  },
+  {
+    "revision": "3da3870d44d42589510e1409d34b94ba",
+    "url": "/portfolio/static/media/homeIcon.3da3870d.svg"
+  },
+  {
+    "revision": "1ef5a30d69a23cffd7fd5ceb2df4569b",
+    "url": "/portfolio/index.html"
+  }
+];
