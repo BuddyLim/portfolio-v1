4c98f37ec7b2cf31cca852bfce05ff9011b45be1
Buddy <buddy.tlimk@gmail.com>
2022-06-10 13:04:44 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d98b906abae566dbe70bd440644ec027.js"
+  "/portfolio/precache-manifest.e3a83795a48bc71070c10a61e644125d.js"
 );
 
 workbox.clientsClaim();
