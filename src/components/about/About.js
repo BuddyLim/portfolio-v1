8f785ad4e7fd0a70dd905d6a41a72dd175a03516
Buddy <buddy.tlimk@gmail.com>
2022-12-09 18:38:42 +0800

    [Add] General resume update and touchup

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,19 +9,20 @@ export default function About({ breakpoint }){
     <div id="about" className="base-section">
       <div className="about">
         <div className="about__heading">
-          <p className="about__title">{"I’m Buddy Lim,\na web developer based in Kuala Lumpur"}</p>
+          <p className="about__title">{"I'm Buddy Lim,\na web developer based in Singapore"}</p>
           <div className="about__description">
             <p>My main expertise lies in web development where I primarily use React for front-end and NodeJS for back-end.</p>
-            <p>I have also studied Game Development during my uni days so I have an understanding of Unity and Unreal. Don’t hesistate to reach out to me!</p>
+            <p>I am also familiar with Unity and Unreal game engines as I have used these engines to develop games during my degree</p>
+            <p>Got anything interesting or something you'd like to share? Feel free to reach out to me!</p>
           </div>
         </div>
         <dl className="about__list">
-          <dt className="about__list-title">What I like</dt>
+          <dt className="about__list-title">My hobbies</dt>
           <dd>Experimenting with new tech</dd>
           <dd className="about__parkour" onClick={() =>{handleLinkClick("https://www.instagram.com/its_thatbuddy/", breakpoint)}}>Parkour</dd>
           <dd>Exploring new environments</dd>
 
-          <dt className="about__list-title">What I'm learning</dt>
+          <dt className="about__list-title">My learnings</dt>
           <dd>Scalable front-end architecture</dd>
           <dd>E2E Browser Testing</dd>
           <dd>Responsive layouts</dd>
