73a706d28265daeb7dd12621e17cb1877997ccf6
Kuang Tar Lim <buddy.tlimk@gmail.com>
2024-07-13 17:53:03 +0800

    Updates

---
+++ precache-manifest.40df5724d757158b2e6f96f92dbf9bab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5cbb99cb22bc51dfbde",
+    "url": "/portfolio/v1/static/css/main.3e5d87ac.chunk.css"
+  },
+  {
+    "revision": "c5cbb99cb22bc51dfbde",
+    "url": "/portfolio/v1/static/js/main.c5cbb99c.chunk.js"
+  },
+  {
+    "revision": "75aa2d861595ad62f4c5",
+    "url": "/portfolio/v1/static/js/1.75aa2d86.chunk.js"
+  },
+  {
+    "revision": "78928dc53c771b159073",
+    "url": "/portfolio/v1/static/css/2.9adb88a8.chunk.css"
+  },
+  {
+    "revision": "78928dc53c771b159073",
+    "url": "/portfolio/v1/static/js/2.78928dc5.chunk.js"
+  },
+  {
+    "revision": "db94a7cf3046ac8ee86e",
+    "url": "/portfolio/v1/static/js/runtime~main.db94a7cf.js"
+  },
+  {
+    "revision": "94208864d506f0dc4e6917ec3187e16b",
+    "url": "/portfolio/v1/static/media/hamburgerIcon.94208864.svg"
+  },
+  {
+    "revision": "c20a95a6aa801cd89aa8576c1fa1db30",
+    "url": "/portfolio/v1/static/media/headerIcon.c20a95a6.svg"
+  },
+  {
+    "revision": "b0493227810de064773e3c783ab0b079",
+    "url": "/portfolio/v1/static/media/closeIcon.b0493227.svg"
+  },
+  {
+    "revision": "3da3870d44d42589510e1409d34b94ba",
+    "url": "/portfolio/v1/static/media/homeIcon.3da3870d.svg"
+  },
+  {
+    "revision": "80e7fe265861655c5451d3e280f2339d",
+    "url": "/portfolio/v1/index.html"
+  }
+];
